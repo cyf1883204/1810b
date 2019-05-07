a4287888ba75a57a994eec332d77b0157740eaad
cyf1883204 <50314693+cyf1883204@users.noreply.github.com>
2019-05-07 11:46:42 +0800

    十一点

--- BookType.java
+++ BookType.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class BookType {                 
   //            shihsid 
     private Integer id;
-
+    //ahahahahah
     private String typeName;
 // sssssss双方都是
   //的发射点犯得上的
